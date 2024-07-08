a8e0efa328b91023f7387f8763eeaf5dabec2755
lemminh54106 <leminh54106@gmail.com>
2024-07-08 17:20:17 +0700

    ss16.

---
+++ src/Searching/binary/Test.java
@@ -0,0 +1,8 @@
+package Searching.binary;
+
+import java.util.Scanner;
+
+public class Test {
+    searching.binary.AlgorithsmOfBinarySearch binarySearch = new searching.binary.AlgorithsmOfBinarySearch();
+    Scanner scanner = new Scanner(System.in);
+}
